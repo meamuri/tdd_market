f422e94a9852bd82faf039f075b85bca75791680
meamuri <meammf@yandex.ru>
2017-03-31 22:32:13 +0300

    now we can delete items

--- src/main/java/data/Market.java
+++ src/main/java/data/Market.java
@@ -3,10 +3,12 @@ package data;
 
 import data.abstracts.Thing;
 
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 
 public class Market{
     LinkedList<Thing> things;
+    LinkedHashSet<Integer> ids;
 
     public Market() {
         things = new LinkedList<Thing>();
@@ -18,5 +20,25 @@ public class Market{
 
     public int count() {
         return things.size();
+    }
+
+    public Thing getItemById(int i) {
+        for (Thing thing: things){
+            if (i == thing.getId())
+                return thing;
+        }
+        return null;
+    }
+
+    public boolean deleteItemById(int i) {
+        int k = 0;
+        for (Thing thing: things) {
+            if (i == thing.getId()) {
+                things.remove(k);
+                return true;
+            }
+            ++k;
+        }
+        return false;
     }
 }
--- src/test/java/unit/MarketTest.java
+++ src/test/java/unit/MarketTest.java
@@ -3,6 +3,8 @@ package unit;
 import data.Market;
 import data.abstracts.Thing;
 import data.things.Car;
+import data.things.Guitar;
+import data.things.Watch;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,13 +15,35 @@ public class MarketTest {
     @Before
     public void setup(){
         market = new Market();
+        market.add(new Car(1, "Bentley", 199216.99, 612));
+        market.add(new Car(2, "RollsRoyce", 252016.99, 599));
+        market.add(new Car(3, "BMW 760Li", 125216.99, 620));
+        market.add(new Car(4, "Audi s8", 125216.99, 540));
+        market.add(new Guitar(5, "Fender", 216.99, 5));
+        market.add(new Watch(6, "Longines", 1216.99, 1));
     }
 
     @Test
     public void userWantAddItem(){
-        Thing thing = new Car(1, "bentley", 125216.99, 612);
-        market.add(thing);
-        Assert.assertEquals(1, market.count());
+        Market m = new Market();
+        m.add(new Car(1, "bentley", 125216.99, 612));
+        Assert.assertEquals(1, m.count());
     }
 
+    @Test
+    public void userWantGetItemById(){
+        market.add(new Car(10, "Mercedes", 2300000,600));
+        Thing th = market.getItemById(10);
+        Assert.assertEquals("Mercedes", th.getTitle());
+        th = market.getItemById(1);
+        Assert.assertEquals("Bentley", th.getTitle());
+        Assert.assertNull(market.getItemById(12512));
+    }
+
+    @Test
+    public void userWantDeleteItemById(){
+        Assert.assertEquals("Fender", market.getItemById(5).getTitle());
+        market.deleteItemById(5);
+        Assert.assertNull(market.getItemById(5));
+    }
 }
